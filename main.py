8f4586ef9fd327060d0ed4f7952ad54ff47e42d9
Medard_Baran <medard.baran@gmail.com>
2024-09-07 19:18:59 +0200

    Add random nick

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import logging
-
+import random
+from db import pool
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
+def get_random_nick():
+    with pool.connect() as db_conn:
+        nicks = db_conn.execute("SELECT * FROM nicks")
+
+    return random.choice(nicks)
+
+
 @app.route('/')
 def hello():
+    nick = get_random_nick()
     return ('Hello Cloud!\n'
             'This app was created for GCP Application Development Challenge\n')
+
 
 @app.errorhandler(500)
 def server_error(e):
@@ -16,6 +27,7 @@ def server_error(e):
     An internal error occurred: <pre>{}</pre>
     See logs for full stacktrace.
     """.format(e), 500
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
